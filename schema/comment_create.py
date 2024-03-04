a20dd41875c4d165f81a58a3ebb149fdfba67d03
Alexfuz59 <Alexfuz59@users.noreply.github.com>
2024-03-04 14:11:42 +0000

    Deploying to gh-pages from @ Alexfuz59/dummyapi@10460e1d5bb0f28f9bb92ca52f2de18e69b443b3 🚀

--- schema/comment_create.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class CommentCreate(BaseModel):
-    message: str = Field(min_length=2, max_length=500)
-    owner: str
-    post: str
--- schema/tags.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-
-class DataListTags(BaseModel):
-    data: List[str]
