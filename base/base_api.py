51b63a43ac9d0fdcb764f2ce516d141b6def0da2
Aleksey <petaleser59@gmail.ru>
2024-04-26 15:36:13 +0300

    add ErrorMessage, update validation response

--- base/base_api.py
+++ base/base_api.py
@@ -1,6 +1,8 @@
 import allure
 import json
 from allure_commons.types import AttachmentType
+from enums.error_message import ErrorMessage
+from pydantic import ValidationError
 
 
 class BaseAPI:
@@ -14,24 +16,27 @@ class BaseAPI:
 
     @allure.step('Check validate schema')
     def check_validate(self):
-        parsed_object = self.SCHEMA.model_validate(self.response_json)
-        return parsed_object
+        try:
+            parsed_object = self.SCHEMA.model_validate(self.response_json)
+            return parsed_object
+        except ValidationError:
+            raise AssertionError(ErrorMessage.ERROR_VALIDATION)
 
     @allure.step('Check response is 200')
     def check_response_is_200(self):
-        assert self.response.status_code == 200, f'Error status code {self.response.status_code}'
+        assert self.response.status_code == 200, ErrorMessage.ERROR_STATUS_CODE(self.response.status_code)
 
     @allure.step('Check response is 404')
     def check_response_is_404(self):
-        assert self.response.status_code == 404, f'Error status code {self.response.status_code}'
+        assert self.response.status_code == 404, ErrorMessage.ERROR_STATUS_CODE(self.response.status_code)
 
     @allure.step('Check total number of objects in the list')
     def check_total_object(self, objectCount, total_object=1):
-        assert total_object == objectCount, 'Wrong total number of objects'
+        assert total_object == objectCount, ErrorMessage.ERROR_TOTAL_OBJECTS
 
     @allure.step('Check response is 404')
     def check_response_is_403(self):
-        assert self.response.status_code == 403, f'Error status code {self.response.status_code}'
+        assert self.response.status_code == 403, ErrorMessage.ERROR_STATUS_CODE(self.response.status_code)
 
     @allure.step('Add json to allure report')
     def attach_response(self):
--- enums/error_message.py
+++ enums/error_message.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 
 class ErrorMessage(Enum):
+    ERROR_STATUS_CODE = lambda status_code: f'Error status code {status_code}'
     ERROR_USER_ID_COMMENT = 'Invalid user ID in comment'
     ERROR_POST_ID_COMMENT = 'Invalid post ID in comment'
     ERROR_POST_ID_COMMENT_LIST = 'Invalid post ID in comment list'
@@ -13,3 +14,5 @@ class ErrorMessage(Enum):
     ERROR_UPDATE_TAGS = 'Error tags update'
     ERROR_FIRSTNAME_UPDATED = 'FirstName has not been updated'
     ERROR_LASTNAME_UPDATED = 'LastName has not been updated'
+    ERROR_TOTAL_OBJECTS = 'Wrong total number of objects'
+    ERROR_VALIDATION = 'Response validation error'
